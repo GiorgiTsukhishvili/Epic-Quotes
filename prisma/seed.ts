fbb3527bd7c04df36973e48e8d962ac1ef427306
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-14 17:04:10 +0400

    add user and email seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 
 const movieTags = [
@@ -19,21 +20,60 @@ const movieTags = [
   { en: "Western", ka: "ვესტერნი" },
 ];
 
-const prismaClient = new PrismaClient();
+const prisma = new PrismaClient();
 
-const main = async () => {
-  await prismaClient.tag.deleteMany();
+const tagSeeder = async () => {
+  await prisma.tag.deleteMany();
 
   try {
     for (let tag of movieTags) {
-      await prismaClient.tag.create({ data: { tag } });
+      await prisma.tag.create({ data: { tag } });
     }
 
     console.log("\x1b[32mMovie tags seed is complete\x1b[32m");
   } catch (err) {
     console.log(err);
+  }
+};
+
+const userAndEmailSeeder = async () => {
+  try {
+    const hashedPassword = await bcrypt.hash("12345678", 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name: "John Doe",
+        password: hashedPassword,
+        image: `${process.env.APP_URL}/imgs/default.png`,
+      },
+    });
+
+    const verificationToken = crypto.randomUUID();
+
+    await prisma.email.create({
+      data: {
+        email: "johndoe@gmail.com",
+        is_primary: true,
+        userId: user.id,
+        verificationToken,
+        emailVerifiedAt: new Date(),
+      },
+    });
+
+    console.log("\x1b[32mUser and Email seed is complete\x1b[32m");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const main = async () => {
+  try {
+    await userAndEmailSeeder();
+    await tagSeeder();
+  } catch (err) {
+    console.log(err);
   } finally {
-    prismaClient.$disconnect();
+    prisma.$disconnect();
   }
 };
 
