f0b423f8842a5fcd33f9bf010613abbbe85270d8
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-14 21:49:00 +0400

    add movie delete logic

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -11,7 +11,18 @@ export const createMovie = (req: Request, res: Response) => {};
 
 export const updateMovie = (req: Request, res: Response) => {};
 
-export const deleteMovie = (req: Request, res: Response) => {};
+export const deleteMovie = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    await prismaClient.user.delete({ where: { id: +id } });
+
+    res.status(204).json({ message: "Movie was deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Could not fetch movie genres");
+  }
+};
 
 export const names = async (req: Request, res: Response) => {
   try {
