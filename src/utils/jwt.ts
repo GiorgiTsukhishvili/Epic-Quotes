d3a40bcb215bfa98dd4e9b889e6322df1e0873a5
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-14 13:16:36 +0400

    add expiration dates to jwt tokens

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -7,13 +7,28 @@ export const generateJWTToken = (
   },
   remember?: boolean
 ) => {
+  const jwtTokenExpirationDate = process.env.JWT_TOKEN_EXPIRATION_DATE;
+  const jwtTokenRefreshExpirationDate =
+    process.env.JWT_REMEMBER_TOKEN_EXPIRATION_DAT;
+
   const accessToken = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: remember ? "256h" : process.env.JWT_EXPIRATION_DATE,
+    expiresIn: remember ? "256h" : jwtTokenExpirationDate,
   });
 
   const refreshToken = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: process.env.JWT_REMEMBER_TOKEN_EXPIRATION_DATE,
+    expiresIn: jwtTokenRefreshExpirationDate,
   });
 
-  return { accessToken, refreshToken };
+  const now = new Date().getTime();
+
+  return {
+    accessToken,
+    refreshToken,
+    tokenExpiresIn: new Date(
+      now + +(jwtTokenExpirationDate ?? 100) * 1000
+    ).getTime(),
+    rememberTokenExpiresIn: new Date(
+      now + +(jwtTokenRefreshExpirationDate ?? 1000) * 1000
+    ).getTime(),
+  };
 };
