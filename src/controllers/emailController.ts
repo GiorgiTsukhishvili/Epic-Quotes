7ddc93cd52d0f08ba0a9b5b4a2c3e7f784a54d21
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-14 21:51:19 +0400

    add email delete logic

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -1,4 +1,7 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+
+const prismaClient = new PrismaClient();
 
 export const addEmail = (req: Request, res: Response) => {};
 
@@ -6,4 +9,15 @@ export const makeEmailPrimary = (req: Request, res: Response) => {};
 
 export const verifyAdditionalEmail = (req: Request, res: Response) => {};
 
-export const deleteEmail = (req: Request, res: Response) => {};
+export const deleteEmail = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    await prismaClient.quote.delete({ where: { id: +id } });
+
+    res.status(204).json({ message: "Email was deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Could not fetch email");
+  }
+};
