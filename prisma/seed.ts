912adbc1e33777230e51f0f67a94374f85fe3ce2
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-14 16:42:17 +0400

    add seeder for movie tags

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const movieTags = [
+  { en: "Action", ka: "ექშენი" },
+  { en: "Adventure", ka: "სათავგადასავლო" },
+  { en: "Animation", ka: "ანიმაცია" },
+  { en: "Comedy", ka: "კომედია" },
+  { en: "Crime", ka: "კრიმინალი" },
+  { en: "Documentary", ka: "დოკუმენტური" },
+  { en: "Drama", ka: "დრამა" },
+  { en: "Fantasy", ka: "ფანტასტიკა" },
+  { en: "Horror", ka: "საშინელებათა" },
+  { en: "Musical", ka: "მუსიკალური" },
+  { en: "Mystery", ka: "მისტიკა" },
+  { en: "Romance", ka: "რომანტიკა" },
+  { en: "Sci-Fi", ka: "მეცნიერული ფანტასტიკა" },
+  { en: "Thriller", ka: "თრილერი" },
+  { en: "War", ka: "ომი" },
+  { en: "Western", ka: "ვესტერნი" },
+];
+
+const prismaClient = new PrismaClient();
+
+const main = async () => {
+  await prismaClient.tag.deleteMany();
+
+  try {
+    for (let tag of movieTags) {
+      await prismaClient.tag.create({ data: { tag } });
+    }
+
+    console.log("\x1b[32mMovie tags seed is complete\x1b[32m");
+  } catch (err) {
+    console.log(err);
+  } finally {
+    prismaClient.$disconnect();
+  }
+};
+
+main();
