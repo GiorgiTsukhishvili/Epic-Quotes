48b67047e097283b4ca455fff78a1a61ebf9ba5c
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-15 13:08:44 +0400

    add auth model

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,65 +1,17 @@
 import { Request, Response } from 'express'
-import bcrypt from 'bcrypt'
-import { PrismaClient } from '@prisma/client'
-import { verificationEmailTemplate } from '../templates/verification-email.template'
-import { emailTranslations } from '../translations/email'
 import { generateJWTToken } from '../utils/jwt'
-import { transporter } from '../config/nodemailer'
-
-const prisma = new PrismaClient()
+import { Register } from '../models/auth.model'
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, name, password } = req.body
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    const user = await prisma.user.create({
-      data: {
-        name,
-        password: hashedPassword,
-        image: `${process.env.APP_URL}/imgs/default.png`,
-      },
-    })
-
-    const verificationToken = crypto.randomUUID()
-
-    await prisma.email.create({
-      data: {
-        email,
-        is_primary: true,
-        userId: user.id,
-        verificationToken,
-      },
-    })
-
     const lang: 'ka' | 'en' = req.body.lang ?? 'en'
 
-    // Generate a temporary signed URL for email verification (30 minutes expiration)
-    const baseUrl = `${process.env.FRONT_URL}/${lang}`
-    const verificationLink = new URL(baseUrl)
-
-    verificationLink.searchParams.append(
-      'register-link',
-      `${process.env.APP_URL}/api/v1/verify/${verificationToken}`
-    )
-
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: req.body.email,
-      subject: 'Account Verification',
-      html: verificationEmailTemplate(
-        lang,
-        verificationLink,
-        user.name,
-        emailTranslations[lang]['joining-text'],
-        emailTranslations[lang]['verify-button'],
-        emailTranslations[lang]['account-verification']
-      ),
-    })
+    const user = await Register.register(name, email, password, lang)
 
     res.status(201).json({
-      user: { name: user.name, email: email.email },
+      user: { name, email },
       tokens: generateJWTToken(
         {
           userId: user.id,
---
+++ src/models/auth.model.ts
@@ -0,0 +1,60 @@
+import { transporter } from '../config/nodemailer'
+import { prisma } from '../config/prisma'
+import { verificationEmailTemplate } from '../templates/verification-email.template'
+import { emailTranslations } from '../translations/email'
+import bcrypt from 'bcrypt'
+
+export class Register {
+  static async register(
+    name: string,
+    email: string,
+    password: string,
+    lang: 'ka' | 'en'
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        password: hashedPassword,
+        image: `${process.env.APP_URL}/imgs/default.png`,
+      },
+    })
+
+    const verificationToken = crypto.randomUUID()
+
+    await prisma.email.create({
+      data: {
+        email,
+        is_primary: true,
+        userId: user.id,
+        verificationToken,
+      },
+    })
+
+    // Generate a temporary signed URL for email verification (30 minutes expiration)
+    const baseUrl = `${process.env.FRONT_URL}/${lang}`
+    const verificationLink = new URL(baseUrl)
+
+    verificationLink.searchParams.append(
+      'register-link',
+      `${process.env.APP_URL}/api/v1/verify/${verificationToken}`
+    )
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Account Verification',
+      html: verificationEmailTemplate(
+        lang,
+        verificationLink,
+        user.name,
+        emailTranslations[lang]['joining-text'],
+        emailTranslations[lang]['verify-button'],
+        emailTranslations[lang]['account-verification']
+      ),
+    })
+
+    return user
+  }
+}
