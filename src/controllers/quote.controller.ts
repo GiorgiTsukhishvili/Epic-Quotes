0aaeae3ed9d954d70c3cdc6af84aef9d34591e35
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-15 15:29:35 +0400

    add ability to fetch quote

--- src/controllers/quote.controller.ts
+++ src/controllers/quote.controller.ts
@@ -3,7 +3,18 @@ import { Quote } from '../models/quote.model'
 
 export const getQuotes = (req: Request, res: Response) => {}
 
-export const getQuote = (req: Request, res: Response) => {}
+export const getQuote = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+
+    const quote = await Quote.find(+id)
+
+    res.status(204).json(quote)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('Could not fetch quote')
+  }
+}
 
 export const createQuote = (req: Request, res: Response) => {}
 
--- src/models/quote.model.ts
+++ src/models/quote.model.ts
@@ -1,6 +1,18 @@
 import { prisma } from '../config/prisma'
 
 export class Quote {
+  static async find(id: number) {
+    return await prisma.quote.findUnique({
+      where: { id },
+      include: {
+        like: { include: { user: true } },
+        comment: {
+          include: { user: true },
+        },
+      },
+    })
+  }
+
   static async delete(id: number) {
     await prisma.quote.delete({ where: { id: +id } })
   }
