4bd1eb955eb64177e925880e945dff03d1952dcc
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-16 15:47:03 +0400

    update add email request

--- src/requests/email.requests.ts
+++ src/requests/email.requests.ts
@@ -4,4 +4,10 @@ export const emailVerificationRequest = [
   body('token').notEmpty().withMessage('Token must not be empty'),
 ]
 
-export const addAdditionalEmailRequest = []
+export const addAdditionalEmailRequest = [
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Must be a valid email address'),
+]
