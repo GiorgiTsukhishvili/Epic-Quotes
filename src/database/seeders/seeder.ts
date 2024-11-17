b984ad9d2337555f1fc3c150f6f3b6896883029e
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-17 14:59:54 +0400

    add seeders to main seeder

--- src/database/seeders/seeder.ts
+++ src/database/seeders/seeder.ts
@@ -2,6 +2,9 @@ import { userAndEmailSeeder } from './userEmail.seeder'
 import { tagSeeder } from './tag.seeder'
 import { movieSeeder } from './movie.seeder'
 import { movieTagSeeder } from './movieTag.seeder'
+import { quoteSeeder } from './quote.seeder'
+import { likeSeeder } from './like.seeder'
+import { commentSeeder } from './comment.seeder'
 
 const main = async () => {
   try {
@@ -9,6 +12,9 @@ const main = async () => {
     await tagSeeder()
     await movieSeeder()
     await movieTagSeeder()
+    await quoteSeeder()
+    await likeSeeder()
+    await commentSeeder()
   } catch (err) {
     console.log(err)
   }
